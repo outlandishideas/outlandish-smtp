b9fd4bf67c69f9f3c21527d21a59b885708f1dce
Rasmus Winter <rasmus@outlandish.com>
2021-03-23 16:37:41 +0000

    Improving thread-safety

--- outlandish-smtp.php
+++ outlandish-smtp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Outlandish SMTP Plugin
  * Plugin URI: https://outlandish.com/
  * Description: Provides a number of different ways to set how WordPress sends emails
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Outlandish
  * Author URI: https://outlandish.com/
  * License: MIT License
@@ -11,35 +11,52 @@
 
 if (getenv('SES_SMTP_USER') && getenv('SES_SMTP_PASS')) {
     // Send using SES
-    add_action('phpmailer_init', function($phpmailer) {
+
+    $user = getenv('SES_SMTP_USER');
+    $pass = getenv('SES_SMTP_PASS');
+    $port = getenv('SES_SMTP_PORT') ?: '587';
+    $host = getenv('SES_SMTP_HOST') ?: 'email-smtp.eu-west-1.amazonaws.com';
+
+    add_action('phpmailer_init', function ($phpmailer) use ($user, $pass, $host, $port) {
         $phpmailer->isSMTP();
-        $phpmailer->Host     = getenv('SES_SMTP_HOST') ?: 'email-smtp.eu-west-1.amazonaws.com';
+        $phpmailer->Host     = $host;
         $phpmailer->SMTPAuth = true;
-        $phpmailer->Port     = getenv('SES_SMTP_PORT') ?: '587';
-        $phpmailer->Username = getenv('SES_SMTP_USER');
-        $phpmailer->Password = getenv('SES_SMTP_PASS');
+        $phpmailer->Port     = $port;
+        $phpmailer->Username = $user;
+        $phpmailer->Password = $pass;
     });
-} else if (getenv('MAILTRAP_USER') && getenv('MAILTRAP_PASS')) {
+} elseif (getenv('MAILTRAP_USER') && getenv('MAILTRAP_PASS')) {
     // Use mailtrap
-    add_action('phpmailer_init', function($phpmailer) {
+
+    $user = getenv('MAILTRAP_USER');
+    $pass = getenv('MAILTRAP_pass');
+
+    add_action('phpmailer_init', function ($phpmailer) use ($user, $pass) {
         $phpmailer->isSMTP();
         $phpmailer->Host = 'smtp.mailtrap.io';
         $phpmailer->SMTPAuth = true;
         $phpmailer->Port = 2525;
-        $phpmailer->Username = getenv('MAILTRAP_USER');
-        $phpmailer->Password = getenv('MAILTRAP_PASS');
+        $phpmailer->Username = $user;
+        $phpmailer->Password = $pass;
     });
-} else if (getenv('SMTP_HOST')) {
+} elseif (getenv('SMTP_HOST')) {
     // Use SMTP_* env variables
-    add_action('phpmailer_init', function($phpmailer) {
+
+    $host = getenv('SMTP_HOST');
+    $port = getenv('SMTP_PORT') ?: '25';
+    $secure = getenv('SMTP_SECURE') ?: '';
+    $user = getenv('SMTP_USER');
+    $pass = getenv('SMTP_PASS');
+
+    add_action('phpmailer_init', function ($phpmailer) use ($user, $pass, $host, $port, $secure) {
         $phpmailer->isSMTP();
-        $phpmailer->Host       = getenv('SMTP_HOST');
-        $phpmailer->Port       = getenv('SMTP_PORT') ?: '25';
-        $phpmailer->SMTPSecure = getenv('SMTP_SECURE') ?: '';
-        if (getenv('SMTP_USER') && getenv('SMTP_PASS')) {
+        $phpmailer->Host       = $host;
+        $phpmailer->Port       = $port;
+        $phpmailer->SMTPSecure = $secure;
+        if ($user && $pass) {
             $phpmailer->SMTPAuth = true;
-            $phpmailer->Username = getenv('SMTP_USER');
-            $phpmailer->Password = getenv('SMTP_PASS');
+            $phpmailer->Username = $user;
+            $phpmailer->Password = $pass;
         }
     });
 }
@@ -71,19 +88,18 @@ add_filter('wp_mail', function ($args) {
 });
 
 // changes what the wp_mail from address is if the constant is set
-add_filter("wp_mail_from", function ( $original_email_address ) {
+add_filter("wp_mail_from", function ($original_email_address) {
     $from_email = getenv('FROM_EMAIL_ADDRESS');
     return $from_email ?: $original_email_address;
 });
 
 // changes what the wp_mail from name is if the constant is set
-add_filter("wp_mail_from_name", function ( $original_from_name ) {
+add_filter("wp_mail_from_name", function ($original_from_name) {
     $from_name = getenv('FROM_EMAIL_NAME');
     return $from_name ?: $original_from_name;
 });
 
 add_action('wp_mail_failed', function ($wp_error) {
-  /** @var WP_Error $wp_error */
-  error_log('Mail Failed: ' . $wp_error->get_error_message());
+    /** @var WP_Error $wp_error */
+    error_log('Mail Failed: ' . $wp_error->get_error_message());
 });
-
